d28207e704cd968bda5b209a619812e50e998b6f
AmolShinde <37936939+AmolRShinde@users.noreply.github.com>
2021-03-29 21:51:41 +0530

    shoppingCart First commit

---
+++ src/app/products.ts
@@ -0,0 +1,20 @@
+export const products = [
+    {
+        "itemName": "Canon EOD 20D",
+        "category": "Camera",
+        "price": 20000,
+        "availableQuantity": 3
+    },
+    {
+        "itemName": "Adidas Light runner Juice it",
+        "category": "Shoes",
+        "price": 2000,
+        "availableQuantity": 2
+    },
+    {
+        "itemName": "Real Apple",
+        "category": "Juice",
+        "price": 50,
+        "availableQuantity": 6
+    }
+]
